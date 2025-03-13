kyle-c-simmons/cilium

commit e08ace202b71d4796c86b70e6e06e8183157d081

bpf: Enforce symmetric routing for endpoints with parent interfaces
In certain setups, such as AWS ENI, it is expected that non-masqueraded
traffic from an endpoint always egresses via a designated interface.
We call this interface its parent interface.

Normally, this is taken care of by routes and assumptions. We have
routes that direct traffic to the parent interface, for pod-to-pod and
CIDRs in the non-masquerade list.

However, there are cases such as when the AWS NLB is configured to run
in "target type: ip" (balance traffic directly to pod IPs) and
with the `preserve_client_ip` option enabled (do not SNAT the source IP)
that the route based approach breaks down. In this case, Cilium sees
world traffic directly to pod IPs, which normally can't happen, except
for the NLB transparent traffic modifications. For ingress traffic
this is not an issue, but return traffic does not hit any of the routes
that would direct it to the parent interface. Nor can we make a route
since every possible public IP can be a source.

As a fix, this commit adds logic to the snat/fwd path to check if we
see reply traffic from an endpoint with a parent interface egressing out
of any interface other than the parent interface. If we do we redirect
the traffic to the parent interface. The location of this logic is
chosen so that its compatible with proxied traffic as well.

Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

bpf/lib/nodeport_egress.h
@@ -306,12 +306,42 @@ static __always_inline int nodeport_snat_fwd_ipv4(struct __ctx_buff *ctx,
 	void *data, *data_end;
 	struct iphdr *ip4;
 	int l4_off, ret;
+	struct endpoint_info *ep;
 
 	if (!revalidate_data(ctx, &data, &data_end, &ip4))
 		return DROP_INVALID;
 
 	snat_v4_init_tuple(ip4, NAT_DIR_EGRESS, &tuple);
 	l4_off = ETH_HLEN + ipv4_hdrlen(ip4);
+
+	if (is_defined(IS_BPF_HOST)) {
+		ep = __lookup_ip4_endpoint(ip4->saddr);
+		if (ep && ep->parent_ifindex && ep->parent_ifindex != NATIVE_DEV_IFINDEX) {
+			/* This packet came from an endpoint with a parent interface and
+			 * it is currently not egressing on its parent interface.
+			 * Check if its a reply packet, if it is, redirect it to the
+			 * parent interface.
+			 */
+			if (ipv4_load_l4_ports(ctx, ip4, l4_off, CT_EGRESS,
+					       (__be16 *)&tuple.dport, NULL) < 0)
+				return DROP_INVALID;
+
+			if (ct_is_reply4(get_ct_map4(&tuple), &tuple)) {
+				/* Look up the parent interface's MAC address and set it as the
+				 * source MAC address of the packet. We will assume the destination
+				 * MAC address is still correct. This assumption only holds if the
+				 * current and parent interfaces are on the same L2 network such as
+				 * in EKS.
+				 */
+				union macaddr smac = NATIVE_DEV_MAC_BY_IFINDEX(ep->parent_ifindex);
+
+				if (eth_store_saddr_aligned(ctx, smac.addr, 0) < 0)
+					return DROP_WRITE_ERROR;
+
+				return ctx_redirect(ctx, ep->parent_ifindex, 0);
+			}
+		}
+	}
 
 	if (lb_is_svc_proto(tuple.nexthdr) &&
 	    nodeport_has_nat_conflict_ipv4(ip4, &target))

bpf/tests/eni_nlb_symetric_routing_host.c
@@ -1,0 +1,203 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#include "common.h"
+
+/* Enable CT debug output */
+#undef QUIET_CT
+
+#define DEBUG
+
+#include <bpf/ctx/skb.h>
+#include "pktgen.h"
+
+/* Enable code paths under test*/
+#define ENABLE_IPV4
+#define ENABLE_NODEPORT
+#define ENABLE_ROUTING
+
+#define PRIMARY_IFACE 1
+#define SECONDARY_IFACE 2
+#define BACKEND_IFACE 123
+
+#define NATIVE_DEV_IFINDEX PRIMARY_IFACE
+
+#define BACKEND_EP_ID 127
+
+#define DEFAULT_ROUTE_MAC mac_one
+#define NODE_MAC mac_two
+#define LOCAL_BACKEND_MAC mac_three
+
+#define ctx_redirect mock_ctx_redirect
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__type(key, __u32);
+	__type(value, __u32);
+	__uint(max_entries, 1);
+} redirect_ifindex_map __section_maps_btf;
+
+static __always_inline __maybe_unused int
+mock_ctx_redirect(const struct __sk_buff *ctx __maybe_unused,
+		  int ifindex __maybe_unused, __u32 flags __maybe_unused)
+{
+	__u32 key = 0;
+	__u32 *value = map_lookup_elem(&redirect_ifindex_map, &key);
+
+	if (value)
+		*value = ifindex;
+
+	return CTX_ACT_REDIRECT;
+}
+
+#include <bpf_host.c>
+
+#include "lib/endpoint.h"
+#include "lib/ipcache.h"
+
+#define TO_NETDEV	0
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PROG_ARRAY);
+	__uint(key_size, sizeof(__u32));
+	__uint(max_entries, 1);
+	__array(values, int());
+} entry_call_map __section(".maps") = {
+	.values = {
+		[TO_NETDEV] = &cil_to_netdev,
+	},
+};
+
+/* Setup for this test:
+ *
+ * +-------World IP---------+    +----------Pod IP---------+
+ * | v4_ext_one:tcp_src_one | -> | v4_pod_one:tcp_dst_one |
+ * +------------------------+    +------------------------+
+ *          ^                            |
+ *          \---------------------------/
+ *
+ * This happens when in ENI mode and we get traffic via a NLB in ip-mode + preserve-client-ip.
+ * NLB NAT'ing is invisible to us, so we effectively see world to pod IP traffic.
+ * We need to make sure that in BPF masquerade mode, we do not masquerade the pod IP for the return traffic
+ * and route it out of the interface associated with the pod IP, even though egress traffic to
+ * the world is normally routed via the primary interface.
+ *
+ * This test simulates an original incoming packet already traversed the ingress path
+ * and was accepted, leaving a CT entry. We act as if the packet here is entering the
+ * cil_to_netdev on the wrong device, and we expect it to be redirected to the correct
+ * secondary device.
+ */
+
+/* Create a boring TCP packet */
+PKTGEN("tc", "eni_nlb_symetric_routing_egress_v4_setup")
+int eni_nlb_symetric_routing_egress_v4_setup_pktgen(struct __ctx_buff *ctx)
+{
+	struct pktgen builder;
+	struct tcphdr *l4;
+	void *data;
+
+	/* Init packet builder */
+	pktgen__init(&builder, ctx);
+
+	l4 = pktgen__push_ipv4_tcp_packet(&builder,
+					  (__u8 *)mac_zero, (__u8 *)mac_zero,
+					  v4_pod_one, v4_ext_one,
+					  tcp_dst_one, tcp_src_one);
+	if (!l4)
+		return TEST_ERROR;
+
+	data = pktgen__push_data(&builder, default_data, sizeof(default_data));
+	if (!data)
+		return TEST_ERROR;
+
+	/* Calc lengths, set protocol fields and calc checksums */
+	pktgen__finish(&builder);
+
+	return 0;
+}
+
+/* Setup IPCache / Endpoint map / CT state according to the scenario */
+SETUP("tc", "eni_nlb_symetric_routing_egress_v4_setup")
+int eni_nlb_symetric_routing_egress_v4_setup_setup(struct __ctx_buff *ctx)
+{
+	struct ipv4_ct_tuple ct = {
+		.daddr = v4_ext_one,
+		.saddr = v4_pod_one,
+		.dport = tcp_dst_one,
+		.sport = tcp_src_one,
+		.nexthdr = IPPROTO_TCP,
+		.flags = 1,
+	};
+	struct ct_state state = {0};
+
+	state.src_sec_id = WORLD_ID;
+
+	endpoint_v4_add_entry(v4_pod_one, BACKEND_IFACE, BACKEND_EP_ID, 0, 0,
+			      SECONDARY_IFACE, (__u8 *)LOCAL_BACKEND_MAC, (__u8 *)NODE_MAC);
+
+	ipcache_v4_add_entry(v4_pod_one, 0, SECLABEL, 0, 0);
+
+	ct_create4(&CT_MAP_TCP4, NULL, &ct, ctx, CT_INGRESS, &state, NULL);
+
+	/* Jump into the entrypoint */
+	tail_call_static(ctx, entry_call_map, TO_NETDEV);
+	/* Fail if we didn't jump */
+	return TEST_ERROR;
+}
+
+CHECK("tc", "eni_nlb_symetric_routing_egress_v4_setup")
+int eni_nlb_symetric_routing_egress_v4_setup_check(const struct __ctx_buff *ctx)
+{
+	void *data;
+	void *data_end;
+	__u32 *status_code;
+	struct iphdr *l3;
+	struct tcphdr *l4;
+	__u32 key = 0;
+	__u32 *redirect_ifindex;
+
+	test_init();
+
+	data = (void *)(long)ctx->data;
+	data_end = (void *)(long)ctx->data_end;
+
+	if (data + sizeof(__u32) > data_end)
+		test_fatal("status code out of bounds");
+
+	status_code = data;
+
+	if (*status_code != TC_ACT_REDIRECT)
+		test_fatal("packet has not been redirected");
+
+	redirect_ifindex = map_lookup_elem(&redirect_ifindex_map, &key);
+	if (!redirect_ifindex)
+		test_fatal("redirect_ifindex not found");
+
+	if (*redirect_ifindex != SECONDARY_IFACE)
+		test_fatal("redirected to ifindex %d, expected %d", *redirect_ifindex,
+			   SECONDARY_IFACE);
+
+	l3 =  data + sizeof(__u32) + sizeof(struct ethhdr);
+
+	if ((void *)l3 + sizeof(struct iphdr) > data_end)
+		test_fatal("l3 out of bounds");
+
+	if (l3->saddr != v4_pod_one)
+		test_fatal("src IP changed");
+
+	if (l3->daddr != v4_ext_one)
+		test_fatal("dest IP changed");
+
+	l4 = (void *)l3 + sizeof(struct iphdr);
+
+	if ((void *)l4 + sizeof(struct tcphdr) > data_end)
+		test_fatal("l4 out of bounds");
+
+	if (l4->source != tcp_dst_one)
+		test_fatal("src TCP port changed");
+
+	if (l4->dest != tcp_src_one)
+		test_fatal("dst TCP port changed");
+
+	test_finish();
+}

commit 8696e9ebb28848c111f5ce7418ab94950a5aa19a

bpf: Use ct_extract_ports4 in nodeport_snat_fwd_ipv4
ICMP packets are possible in that context, but ipv4_load_l4_ports
expects a service protocol (it parses the first 4 bytes of the L4 header
as the source port and destination port).

Use ct_extract_ports4 instead, which falls back to ipv4_load_l4_ports
for service protocols, and encodes the ICMP echo identifier as the port
in a way understandable by our conntrack. This was ct_is_reply4 won't be
fed garbage when handling an ICMP packet.

Fixes: e08ace202b71 ("bpf: Enforce symmetric routing for endpoints with parent interfaces")
Signed-off-by: Maxim Mikityanskiy <maxim@isovalent.com>

bpf/lib/nodeport_egress.h
@@ -323,11 +323,12 @@ static __always_inline int nodeport_snat_fwd_ipv4(struct __ctx_buff *ctx,
 			 * Check if its a reply packet, if it is, redirect it to the
 			 * parent interface.
 			 */
-			if (ipv4_load_l4_ports(ctx, ip4, l4_off, CT_EGRESS,
-					       (__be16 *)&tuple.dport, NULL) < 0)
-				return DROP_INVALID;
+			ret = ct_extract_ports4(ctx, ip4, l4_off, CT_EGRESS, &tuple, NULL);
+			if (ret < 0 && ret != DROP_CT_UNKNOWN_PROTO)
+				return ret;
 
-			if (ct_is_reply4(get_ct_map4(&tuple), &tuple)) {
+			if (ret != DROP_CT_UNKNOWN_PROTO &&
+			    ct_is_reply4(get_ct_map4(&tuple), &tuple)) {
 				/* Look up the parent interface's MAC address and set it as the
 				 * source MAC address of the packet. We will assume the destination
 				 * MAC address is still correct. This assumption only holds if the